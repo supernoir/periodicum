f1813ad9ca904e47c2720bae5b088b12581d8db1
supernoir <kawohl.bentodesign@gmail.com>
2016-03-24 17:51:08 +0100

    adds nano, creates db request

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 'use strict'
 
+// --- Setup
 var express = require('express');
 var app = express();
+var nano = require('nano')('http://localhost:5984');
 
+
+// --- DB Driver
+var periodicum = nano.db.use('periodicum');
+
+periodicum.list(function(err, body) {
+  if (!err) {
+    body.rows.forEach(function(doc) {
+        
+      console.log(doc);
+    });
+  }
+});
+
+
+
+// --- Listening
 var port = 8787;
 app.listen(port);
 console.log("Server is running at port " + port);
+
+
