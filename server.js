2bb74433915a3b6b2ca1c04a1f0e9c702e613a2a
supernoir <kawohl.bentodesign@gmail.com>
2016-03-24 18:09:07 +0100

    creates GET request

--- server.js
+++ server.js
@@ -3,19 +3,24 @@
 // --- Setup
 var express = require('express');
 var app = express();
-var nano = require('nano')('http://localhost:5984');
+var nano = require('nano')('http://localhost:5984'),
+    params = {include_docs: true}
+    ;
 
 
 // --- DB Driver
 var periodicum = nano.db.use('periodicum');
 
-periodicum.list(function(err, body) {
-  if (!err) {
+app.get('/', function(request, response) {
+
+periodicum.list(params, function(err, body) {
+    var docs = [];
+    
     body.rows.forEach(function(doc) {
-        
-      console.log(doc);
+        docs.push(doc);
     });
-  }
+       response.json(docs);
+    });
 });
 
 
