ca406bd900a55a61d0ad2a98f32cfe6663f91a11
supernoir <kawohl.bentodesign@gmail.com>
2016-03-26 02:14:36 +0100

    gets React to iterate over Array

--- app/app.js
+++ app/app.js
@@ -1,3 +1,35 @@
 'use strict'
 
-console.log("App.js has loaded.");
+var myJSON = [
+    {"name":"Aurum","symbol":"Au"},
+    {"name":"Hydrogen","symbol":"H"}
+    ];
+
+
+
+
+var TheElement = React.createClass({
+  render: function() {
+    return (
+      <div className="TheElement">
+        <div className="card">
+            <div className="card-content">
+                <div className="content">
+                { this.props.allElements.map(function(item) {
+                    return <div>{item}</div>
+                    })
+                }
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var allElements = ["Aurum","Magnesium","Hydrogen"];
+
+ReactDOM.render(
+  <TheElement allElements={ allElements }/>,
+  document.getElementById('ElementComponent')
+);
