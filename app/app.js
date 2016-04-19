bd08a9b2384328c0ff5108d5e2b2a782fb7de76a
Katrin Marschner <katrin.marschner@gmail.com>
2016-04-19 18:18:00 +0200

    add SingleElement to PeriodicTable

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 var PeriodicTable = React.createClass({
   getInitialState: function() {
-    return {data: []};
+    return {data: [], visible: 'index'};
   },
   loadElementsFromServer: function() {
     $.ajax({
@@ -21,10 +21,18 @@ var PeriodicTable = React.createClass({
     this.loadElementsFromServer();
     setInterval(this.loadElementsFromServer, this.props.pollInterval);
   },
+  openSingleElement: function() {
+    this.setState({visible: 'element'});
+  },
+  closeSingleElement: function() {
+    this.setState({visible: 'index'});
+  },
   render: function() {
-    return (
-        <ElementList data={this.state.data} />
-    );
+    if (this.state.visible === 'index') {
+      return <ElementList data={this.state.data} openSingleElement={this.openSingleElement}/>
+    } else {
+      return <SingleElement closeSingleElement={this.closeSingleElement}/>
+    }
   }
 });
 
@@ -44,7 +52,7 @@ var ElementList = React.createClass({
     }
 
     return (
-      <div className="periodic-table">
+      <div className="periodic-table" onClick={this.props.openSingleElement}>
         <div className="row one">
           {elementNodes.slice(0,1)}
           {empty_fields.slice(0,16)}
@@ -89,11 +97,59 @@ var ElementList = React.createClass({
 var Element = React.createClass({
   render: function() {
     return (
-      <a href="single-element.html" className="element" id={this.props.group}>
+      <a className="element" id={this.props.group}>
         <p className="symbol">{this.props.symbol}</p>
         <p className="atomic-number">{this.props.number}</p>
         <p className="name">{this.props.name}</p>
       </a>
+    );
+  }
+});
+
+var SingleElement = React.createClass({
+  render: function() {
+    return (
+      <div className="single-element element-content">
+        <a onClick={this.props.closeSingleElement} className="close"><i className="fa fa-times" aria-hidden="true"></i>
+</a>
+        <div className="element-header">
+          <div className="element-header-image">
+            <img src="img/xenon.jpg" className="element-image" />
+          </div>
+          <div className="element-header-content">
+            <h1>Xenon</h1>
+            <table className="element-table">
+              <tbody>
+                <tr className="element-table-row">
+                  <td className="element-table-cell-label">Symbol</td>
+                  <td className="element-table-cell">Xe</td>
+                </tr>
+                <tr className="element-table-row">
+                  <td className="element-table-cell-label">Group</td>
+                  <td className="element-table-cell">Noble Gas</td>
+                </tr>
+                <tr className="element-table-row">
+                  <td className="element-table-cell-label">Number</td>
+                  <td className="element-table-cell">54</td>
+                </tr>
+                <tr className="element-table-row">
+                  <td className="element-table-cell-label">Molar</td>
+                  <td className="element-table-cell">131.293</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="element-body">
+          <p>Xenon is a chemical element with symbol Xe and atomic number 54. It is a colorless, dense, odorless noble gas, that occurs in the Earth's atmosphere in trace amounts. Although generally unreactive, xenon can undergo a few chemical reactions such as the formation of xenon hexafluoroplatinate, the first noble gas compound to be synthesized. Xenon is used in flash lamps and arc lamps, and as a general anesthetic. Xenon is also being used to search for hypothetical weakly interacting massive particles and as the propellant for ion thrusters in spacecraft. Naturally occurring xenon consists of eight stable isotopes.</p>
+        </div>
+        <div className="element-footer">
+          <p>
+            <a href="#" className="element-tag">Noble Gas</a>
+            <a href="#" className="element-tag">Flourescent</a>
+          </p>
+        </div>
+      </div>
     );
   }
 });
