d46c18f33d08dd93f6bd5d8bf69272ea09268e8c
supernoir <kawohl.bentodesign@gmail.com>
2016-04-19 13:33:25 +0200

    adds filter js, makes filter react component

---
+++ app/filter.js
@@ -0,0 +1,70 @@
+'use strict'
+
+var Filter = React.createClass({
+  render: function() {
+    return (
+<form className="filter-form">
+            <div id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label-all">All Elements</span>
+            </div>
+            <hr id="hr-primary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Noble Gases</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Alkali Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Alkaline Earth Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Transition Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Post-Transition Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Polyatomic Non-Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Diatomic Non-Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Metalloids</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Actinides</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+                <input type="radio" id="input-radio" name="filter-select" value="all" />
+                <span id="filter-label">Lanthanides</span>
+            </label>
+        </form>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Filter />,
+  document.getElementById('filterComponent')
+);
+
