3b05224581d8fc3da6282684ddbd14e8d40efce1
Katrin Marschner <katrin.marschner@gmail.com>
2016-05-17 16:26:28 +0200

    move filter into periodic table

--- app/app.js
+++ app/app.js
@@ -29,7 +29,12 @@ var PeriodicTable = React.createClass({
   },
   render: function() {
     if (this.state.visible === 'index') {
-      return <ElementList data={this.state.data} openSingleElement={this.openSingleElement} />
+      return (
+        <div className="container">
+          <Filter />
+          <ElementList data={this.state.data} openSingleElement={this.openSingleElement} />
+        </div>
+      );
     } else {
       return <SingleElement closeSingleElement={this.closeSingleElement} name={this.state.element.name}
               symbol={this.state.element.symbol} group={this.state.element.group}
@@ -243,6 +248,89 @@ var SingleElement = React.createClass({
           <p>
             <a href="#" className="element-tag">{this.props.group}</a>
           </p>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+// Filter
+
+var Filter = React.createClass({
+  allElementsClass: function(){
+    $('.element').removeClass('mute-element');
+    $('.noble-gas').removeClass("noble-gas-highlighted");
+    $('.alkali-metal').removeClass("alkali-metal-highlighted");
+  },
+  nobleGasClass: function(){
+    $('.element').addClass('mute-element');
+    $('.noble-gas').toggleClass("noble-gas-highlighted");
+  },
+  alkaliMetalClass: function(){
+    $('.element').addClass('mute-element');
+    $('.alkali-metal').toggleClass("alkali-metal-highlighted");
+  },
+  render: function() {
+    return (
+      <div className="filter">
+        <div id="filterComponent">
+          <form className="filter-form">
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="all-elements" value="all" onClick={this.allElementsClass} />
+              <span id="filter-label-all">All Elements</span>
+            </label>
+            <hr id="hr-primary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="noble-gas" value="all" onClick={this.nobleGasClass} />
+              <span id="filter-label">Noble Gases</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="alkali-metal" value="all" onClick={this.alkaliMetalClass} />
+              <span id="filter-label">Alkali Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Alkaline Earth Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Transition Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Post-Transition Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Polyatomic Non-Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Diatomic Non-Metals</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Metalloids</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Actinides</span>
+            </label>
+            <hr id="hr-secondary" />
+            <label id="radio-element">
+              <input type="radio" id="input-radio" name="filter-select" value="all" />
+              <span id="filter-label">Lanthanides</span>
+            </label>
+          </form>
         </div>
       </div>
     );
